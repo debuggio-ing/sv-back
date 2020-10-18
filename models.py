48f3f4ad844eb03648b0c670719fcac5f3fe7a30
Nicolas Luduena <luduena.nicolas.victorio@gmail.com>
2020-10-18 10:03:52 -0300

    SVM-15 db draft, solid user-img-player-lobby relationships

--- models.py
+++ models.py
@@ -12,35 +12,52 @@ class User(db.Entity):
     password = Required(str, hidden=True)
     image = Optional('Image')
     players = Set('Player')
-    
+
 
 class Image(db.Entity):
     id = PrimaryKey(int, auto=True)
     height = Required(int)
     width = Required(int)
     filename = Required(str, unique=True)
-    user = Required(User)
+    user = Required('User')
+
 
 class Player(db.Entity):
     id = PrimaryKey(int, auto=True)
     alive = Required(bool)
-    secret_role = Required(int)
-    position = Required(int)
+    order = Required(int)
+    secret_role = Optional('Role')
     minister = Required(bool)
-    Director = Required(bool)
-    user = Required(User)
+    director = Required(bool)
+    user = Required('User')
     lobby = Required('Lobby')
+    composite_key(user, lobby)
+
+
+class Role(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    player = Required('Player')
+
+
+class Voldemort(Role):
+    pass
+
+
+class Phoenix(Role):
+    pass
+
+
+class DeathEatear(Role):
+    pass
 
 
 class Lobby(db.Entity):
     id = PrimaryKey(int, auto=True)
     name = Required(str)
-    num_players = Required(int)
     max_players = Required(int)
-    initial_card_order = Required(str)
-    player_order = Required(str)
+#    initial_card_order = Required(str)
     creation_date = Required(datetime)
-    players = Set(Player)
+    players = Set('Player')
     match = Optional('Match')
     chat = Optional('Chat')
 
@@ -48,14 +65,14 @@ class Lobby(db.Entity):
 class Chat(db.Entity):
     id = PrimaryKey(int, auto=True)
     text = Required(str)
-    lobby = Required(Lobby)
+    lobby = Required('Lobby')
 
 
 class Match(db.Entity):
     id = PrimaryKey(int, auto=True)
-    game_score = Required(str)
-    semaphore = Optional(str)
-    lobby = Required(Lobby)
+    game_score = Required(str)  # rethink
+    semaphore = Optional(str)  # rethink
+    lobby = Required('Lobby')
     procl_pool = Optional('Procl_pool')
 
 
@@ -64,15 +81,4 @@ class Procl_pool(db.Entity):
     discarded = Required(bool)
     phoenix = Required(bool)
     order = Required(str)
-    match = Required(Match)
-
-
-
-db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
-db.generate_mapping(create_tables=True)
-set_sql_debug(True)
-#@db_session
-#def hola():
-#    u1 = User(mail='laumail', username='lau', password='123456', players=set())
-#    #commit()
-#    select(p for p in User).show()
+    match = Required('Match')
