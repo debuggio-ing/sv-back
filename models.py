03ff330f9cbbe99882da38baa56bced3dab5c08e
Nicolás Ludueña <47897388+nicolasLuduena@users.noreply.github.com>
2020-10-19 21:51:36 -0300

    Svm 15 (#2)
    * SVM-15 hello-word
    
    * SVM-15 draft db models
    
    * SVM-15 db draft
    
    * SVM-15 db draft, solid user-img-player-lobby relationships
    
    * SVM-15 new roles and cards model
    
    * SVM-15 fixed lobby~player + comments
    
    Co-authored-by: aekyos <22533437+aekyos@users.noreply.github.com>

--- models.py
+++ models.py
@@ -1,11 +1,85 @@
+from datetime import datetime
+from sys import maxsize
 from pony.orm import *
+
 
 db = Database()
 
-# PonyORM User class.
+
+# Created when a new user is registered
 class User(db.Entity):
-    mail = Required(str)
-    username = Required(str)
+    id = PrimaryKey(int, auto=True)
+    mail = Required(str, unique=True)
+    username = Required(str, unique=True)
     password = Required(str)
+    image = Optional('Image')
+    players = Set('Player')
+
+
+# Created when the user uploads a profile image
+class Image(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    height = Required(int)
+    width = Required(int)
+    filename = Required(str, unique=True)
+    user = Required('User')
+
+
+# Created when the user joins a match
+class Player(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    alive = Required(bool)
+    order = Required(int)
+    secret_role = Optional('Role')
+    minister = Required(bool)
+    director = Required(bool)
+    user = Required('User')
+    lobby = Required('Lobby')
+    composite_key(user, lobby)
+
+
+# Created when the user is assigned a role
+class Role(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    player = Required('Player')
+    voldemort = Required(bool)
+    phoenix = Required(bool)
+
+
+# Created as a new game
+class Lobby(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    name = Required(str)
+    max_players = Required(int)
+    creation_date = Required(datetime)
+    players = Set('Player')
+    match = Optional('Match')
+    chat = Optional('Chat')
+
+
+# Created with a new game
+class Chat(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    text = Required(str)
+    lobby = Required('Lobby')
+
+
+# Created when a game is started
+class Match(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    semaphore = Required(int, min=0, max=3)
+    lobby = Required('Lobby')
+    cards = Set('Card')
+
+
+# Created when a game is started
+class Card(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    discarded = Required(bool)
+    proclaimed = Required(bool)
+    order = Required(int)
+    is_phoenix = Required(bool)
+    match = Required('Match')
+
 
 db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
