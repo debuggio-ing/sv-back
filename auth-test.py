463b788aa05cd895bebf9d3df495490b878419e4
Lautaro Lecumberry <lautarolecumberry@gmail.com>
2020-10-20 19:48:38 -0300

    SVM-20 Add auth tests

--- auth-test.py
+++ auth-test.py
@@ -1,20 +1,35 @@
-import requests
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from auth import *
 
-PROTECTED_ENDPOINT = 'http://127.0.0.1:8000/protected'
-LOGIN_ENDPOINT = 'http://127.0.0.1:8000/login'
+client = TestClient(app)
 
-token = "Bearer not_valid_jwt_access_token"
 
-with requests.Session() as s:
-    r1 = requests.get(PROTECTED_ENDPOINT, headers={"Authorization": token})
+def test_access_protected_endpoint_unlogged():
+    response = client.get("/protected")
 
-# r.text == {"detail":"Not enough segments"}
-print(r1.text)
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Missing Authorization Header"}
 
-# r2 = requests.post(LOGIN_ENDPOINT, data = , headers={"Content-Type":"application/json"})
 
-payload = {"username":"test","password":"test"}
-with requests.Session() as s:
-    r2 = s.post(LOGIN_ENDPOINT, data=payload, headers={"Content-Type":"application/json"})
+def test_login():
+    response = client.post("/login",
+                           headers={"Content-Type": "application/json"},
+                           json={"username": "test", "password": "test"})
 
-print(r2.content)
+    assert response.status_code == 200
+    assert len(response.json()["access_token"]) == 275
+
+
+def test_access_protected_endpoint_logged():
+    login = client.post("/login",
+                        headers={"Content-Type": "application/json"},
+                        json={"username": "test", "password": "test"})
+
+    token = "Bearer " + login.json()["access_token"]
+
+    response = client.get("/protected",
+                          headers={"Authorization": token}, )
+
+    assert response.status_code == 200
+    assert response.json() == {"logged_in_as": "test"}
