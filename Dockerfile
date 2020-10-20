e2d211d1e5568fd0b18af0d379e78e747b2f4204
mauroebordon <mauro.e.bordon@gmail.com>
2020-10-20 17:58:38 -0300

    SVM-29 last fix hopefully

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.7
-
-EXPOSE 80
-
-COPY ./app /app
-
-RUN pip install -r app/requirements.txt
-
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80", "--reload"]
--- schemas.py
+++
@@ -1,8 +0,0 @@
-from typing import List, Optional
-from pydantic import BaseModel
-
-# Create pydantic user schema for input parsing.
-class UserSchema(BaseModel):
-    email: str
-    username: str
-    password: str
