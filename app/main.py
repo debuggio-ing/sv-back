25c3e1a6fa9b2ea4aaacdb20f7b5a469b3da549a
mauroebordon <mauro.e.bordon@gmail.com>
2020-10-20 13:19:31 -0300

    SVM-23 agregados los __init__.py para poder realizar imports entre los diferentes archivos

--- app/main.py
+++ app/main.py
@@ -1,24 +1,6 @@
-<<<<<<< HEAD
-from typing import Optional
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-=======
 from fastapi import Depends, FastAPI, HTTPException
-from pony.schemas import *
-from ..pony.models import *
-from ..pony.crud import *
-from ..api.routes import *
+
+from api.routes import *
 
 
 app = routes
->>>>>>> SVM-23 La partición de directorios que me parece correcta
