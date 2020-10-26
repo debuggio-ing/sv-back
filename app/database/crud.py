e5dbcd099b1c634515e818aa3dec734810308fd5
Nicolas Luduena <luduena.nicolas.victorio@gmail.com>
2020-10-26 18:17:55 -0300

    fix last commit

--- app/database/crud.py
+++ app/database/crud.py
@@ -4,12 +4,9 @@ from app.database.models import *
 from app.api.schemas import *
 from app.api.hasher import *
 
-def create_db(testing):
-    if (testing):
-        db.bind(provider='sqlite', filename='testing1.sqlite', create_db=True)
-    else:
-        db.bind(provider='sqlite', filename='testing2.sqlite', create_db=True)
-    db.generate_mapping(create_tables=True)
+
+db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
+db.generate_mapping(create_tables=True)
 
 
 # Insert user into the database.
@@ -256,17 +253,19 @@ def get_lobby_max_players(lobby_id: int):
 
     return max_players
 
-
-
 # Get all lobbies ids.
 @db_session
 def get_all_lobbies_ids(lobby_from: Optional[int], lobby_to: Optional[int]):
-    if lobby_to == None:
-        lobby_to = max(l.id for l in Lobby)
-    
-    if lobby_to == None:
+    max_id = max(l.id for l in Lobby)
+    if lobby_to is None and max_id is not None:
+        # If there's an active lobby, set lobby_to = max_id.
+        lobby_to = max_id
+    elif lobby_to is None and max_id is None:
+        # If there's no active lobby, set lobby_to = 0.
         lobby_to = 0
 
-    lobbies_ids = list(select(l.id for l in Lobby if l.id >= lobby_from and l.id <= lobby_to))
+    # Get all lobies with id within range.
+    lobbies_ids = list(select(l.id for l in Lobby if
+                              l.id >= lobby_from and l.id <= lobby_to))
 
     return lobbies_ids
