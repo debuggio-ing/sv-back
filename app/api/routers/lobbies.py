b6f0be7168a95a82fdc1e2c3ca8f5c888ca193f0
Lautaro Lecumberry <lautarolecumberry@gmail.com>
2020-10-26 19:38:02 -0300

    SVM-59 comment code

--- app/api/routers/lobbies.py
+++ app/api/routers/lobbies.py
@@ -8,9 +8,12 @@ from app.api.schemas import *
 from app.database.models import *
 from app.database.crud import *
 
-r = lobbies_router = APIRouter()
 
-# Ver la lista de salas
+# Authentication endpoints' router
+r = auth_router = APIRouter()
+
+
+# Return lobbies list.
 @r.get("/lobbies/", response_model=List[LobbyPublic])
 def get_lobby_list(
     lobby_from: Optional[int] = 0,
@@ -23,21 +26,21 @@ def get_lobby_list(
     lobbies = []
     for lid in required_lobbies:
         lobby = LobbyPublic(id=lid, name=get_lobby_name(lid),
-                    current_players=get_lobby_player_list(lid),
-                    max_players=get_lobby_max_players(lid))
+                            current_players=get_lobby_player_list(lid),
+                            max_players=get_lobby_max_players(lid))
         lobbies.append(lobby)
 
     return lobbies
 
 
-# Ver la información de mi sala
+# Return lobby_id lobby information.
 @r.get("/lobbies/{lobby_id}/", response_model=LobbyPublic)
 def get_lobby(lobby_id: int, Authorize: AuthJWT = Depends()):
     Authorize.jwt_required()
     return
 
 
-# Crear una nueva sala
+# Create new lobby.
 @r.post("/lobbies/new/",
         response_model=LobbyPublic,
         status_code=status.HTTP_201_CREATED)
@@ -58,18 +61,18 @@ def create_lobby(new_lobby: LobbyReg, Authorize: AuthJWT = Depends()):
 
     return lobby
 
-# Unirse a una sala
-# la información del usuario se obtiene del JWT
 
-
-@r.post("/lobbies/{lobby_id}/join/", response_model=LobbyPublic
-        )
+# Join lobby_id lobby.
+@r.post("/lobbies/{lobby_id}/join/",
+        response_model=LobbyPublic)
 def join_game(lobby_id: int, Authorize: AuthJWT = Depends()):
     Authorize.jwt_required()
 
+    # Get information from jwt_token.
     user_email = Authorize.get_jwt_identity()
     player_id = insert_player(user_email=user_email, lobby_id=lobby_id)
 
+    # If player is already in lobby return exception.
     if player_id == -1:
         raise HTTPException(status_code=409, detail="User already in lobby.")
 
@@ -85,7 +88,7 @@ def join_game(lobby_id: int, Authorize: AuthJWT = Depends()):
     return lobby
 
 
-# Empezar la partida
+# Start lobby_id lobby.
 @r.post("/lobbies/{lobby_id}/start/")
 def start_game(
         lobby_id: int,
