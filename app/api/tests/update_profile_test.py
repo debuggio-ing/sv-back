ac810f9af6a5a57306f77f3354379fb417aa4ba3
Nicolás Ludueña <nicolas.luduena@mi.unc.edu.ar>
2020-11-14 01:49:01 -0300

    SVM-145 POST and GET profile picture tested

--- app/api/tests/update_profile_test.py
+++ app/api/tests/update_profile_test.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from app.test import test_svapi
 from app.database.crud import *
+from io import BytesIO
+from PIL import Image
+import os
 
 
 # It's necessary to remove database before running the tests.
@@ -99,3 +102,32 @@ def test_modify_nickname():
         json={"email": user1.email, "password": password2})
 
     assert login1.status_code == 200
+
+
+# Update user's profile picture
+def test_modify_picture():
+    login = testc.post(
+        "api/login/",
+        headers={"Content-Type": "application/json"},
+        json={"email": user1.email, "password": password2})
+    token = "Bearer " + login.json()["access_token"]
+    file_path = ''
+    # test request
+    # with open(fpath, "wb") as f:
+    #     response = client.post("/", files={"file": ("filename", f, "image/jpeg")})
+
+    empty_picture = testc.get("/api/users/picture/", headers={"Authorization": token})
+    assert empty_picture.status_code == 200
+    assert empty_picture.content == b''
+    for i in [1, 2]:
+        with open("app/api/tests/test_pic{}.jpg".format(i), "rb") as image:
+            raw_image = image.read()
+            # comments are for testing manually, it opens the picture
+            # image = Image.open(BytesIO(raw_image)).convert("RGBA")
+            # image.show()
+            response = testc.post("/api/users/picture/", headers={"Authorization": token}, files={"file": raw_image})
+            assert response.status_code == 200
+
+            image = testc.get("/api/users/picture/", headers={"Authorization": token})
+            assert image.status_code == 200
+            assert image.content == raw_image
