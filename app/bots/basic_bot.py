40f92fa2d178a7c1fab314c09b793c2429248a4c
mauroebordon <mauro.e.bordon@gmail.com>
2020-11-30 22:34:38 -0300

    setting local env

--- app/bots/basic_bot.py
+++ app/bots/basic_bot.py
@@ -17,7 +17,7 @@ heroku_path = "https://secret-voldemort-back.herokuapp.com/api/"
 local_path = "http://localhost:8000/api/"
 
 
-is_local = False
+is_local = True
 basepath = local_path
 if not is_local:
     basepath = heroku_path
